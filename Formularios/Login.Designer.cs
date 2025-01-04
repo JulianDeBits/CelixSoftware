fdf7807f9ed5601f3ce50a1eb3b360efaa88358f
JulianDeBits <julianquinterortiz@gmail.com>
2025-01-04 10:28:59 -0500

    Adelantos en Login

--- Formularios/Login.Designer.cs
+++ Formularios/Login.Designer.cs
@@ -28,112 +28,107 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.ImagenBienvenida = new System.Windows.Forms.PictureBox();
+            this.panel1 = new System.Windows.Forms.Panel();
             this.label1 = new System.Windows.Forms.Label();
-            this.Usuariotext = new System.Windows.Forms.TextBox();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.button1 = new System.Windows.Forms.Button();
+            this.txtusuario = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
+            this.txtpassword = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
-            ((System.ComponentModel.ISupportInitialize)(this.ImagenBienvenida)).BeginInit();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // ImagenBienvenida
+            // panel1
             // 
-            this.ImagenBienvenida.BackColor = System.Drawing.Color.Black;
-            this.ImagenBienvenida.Location = new System.Drawing.Point(0, -2);
-            this.ImagenBienvenida.Name = "ImagenBienvenida";
-            this.ImagenBienvenida.Size = new System.Drawing.Size(444, 563);
-            this.ImagenBienvenida.TabIndex = 0;
-            this.ImagenBienvenida.TabStop = false;
+            this.panel1.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(250, 329);
+            this.panel1.TabIndex = 0;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Candara", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(501, 40);
+            this.label1.Font = new System.Drawing.Font("Segoe UI Semibold", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(449, 29);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(342, 59);
+            this.label1.Size = new System.Drawing.Size(181, 32);
             this.label1.TabIndex = 1;
             this.label1.Text = "Inicio de Sesión";
             // 
-            // Usuariotext
+            // txtusuario
             // 
-            this.Usuariotext.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.Usuariotext.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            this.Usuariotext.Font = new System.Drawing.Font("Candara", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Usuariotext.ForeColor = System.Drawing.Color.Black;
-            this.Usuariotext.Location = new System.Drawing.Point(533, 205);
-            this.Usuariotext.Name = "Usuariotext";
-            this.Usuariotext.Size = new System.Drawing.Size(277, 23);
-            this.Usuariotext.TabIndex = 2;
-            // 
-            // textBox1
-            // 
-            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.textBox1.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            this.textBox1.Font = new System.Drawing.Font("Candara", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox1.ForeColor = System.Drawing.Color.Black;
-            this.textBox1.Location = new System.Drawing.Point(533, 313);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.PasswordChar = '*';
-            this.textBox1.Size = new System.Drawing.Size(277, 23);
-            this.textBox1.TabIndex = 3;
-            // 
-            // button1
-            // 
-            this.button1.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.button1.Font = new System.Drawing.Font("Candara", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.ForeColor = System.Drawing.Color.Black;
-            this.button1.Location = new System.Drawing.Point(533, 420);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(277, 36);
-            this.button1.TabIndex = 4;
-            this.button1.Text = "Ingresar";
-            this.button1.UseVisualStyleBackColor = false;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.txtusuario.AcceptsReturn = true;
+            this.txtusuario.BackColor = System.Drawing.Color.AliceBlue;
+            this.txtusuario.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtusuario.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtusuario.Location = new System.Drawing.Point(362, 112);
+            this.txtusuario.Multiline = true;
+            this.txtusuario.Name = "txtusuario";
+            this.txtusuario.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.txtusuario.Size = new System.Drawing.Size(332, 20);
+            this.txtusuario.TabIndex = 2;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Candara", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(529, 183);
+            this.label2.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(359, 92);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(67, 19);
-            this.label2.TabIndex = 5;
+            this.label2.Size = new System.Drawing.Size(56, 17);
+            this.label2.TabIndex = 3;
             this.label2.Text = "Usuario:";
+            // 
+            // txtpassword
+            // 
+            this.txtpassword.AcceptsReturn = true;
+            this.txtpassword.BackColor = System.Drawing.Color.AliceBlue;
+            this.txtpassword.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtpassword.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtpassword.Location = new System.Drawing.Point(362, 187);
+            this.txtpassword.Multiline = true;
+            this.txtpassword.Name = "txtpassword";
+            this.txtpassword.PasswordChar = '*';
+            this.txtpassword.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.txtpassword.Size = new System.Drawing.Size(332, 20);
+            this.txtpassword.TabIndex = 3;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Candara", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(529, 291);
+            this.label3.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(359, 167);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(81, 19);
-            this.label3.TabIndex = 5;
+            this.label3.Size = new System.Drawing.Size(65, 17);
+            this.label3.TabIndex = 3;
             this.label3.Text = "Password:";
+            // 
+            // button1
+            // 
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.Location = new System.Drawing.Point(362, 262);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(332, 27);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Ingresar";
+            this.button1.UseVisualStyleBackColor = true;
             // 
             // Login
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(884, 561);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.label2);
+            this.ClientSize = new System.Drawing.Size(784, 329);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.Usuariotext);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtpassword);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtusuario);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.ImagenBienvenida);
+            this.Controls.Add(this.panel1);
             this.Name = "Login";
+            this.Opacity = 0.95D;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Login";
-            ((System.ComponentModel.ISupportInitialize)(this.ImagenBienvenida)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -141,13 +136,13 @@
 
         #endregion
 
-        private System.Windows.Forms.PictureBox ImagenBienvenida;
+        private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox Usuariotext;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtusuario;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtpassword;
         private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
     }
 }
 
