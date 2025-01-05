6615c8e4cb5d5841c6b63e24d2e1c362c9f241c4
JulianDeBits <julianquinterortiz@gmail.com>
2025-01-05 11:43:54 -0500

    Logica

---
+++ logica/Autenticacion.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using Microsoft.Analytics.Interfaces;
+using Microsoft.Analytics.Types.Sql;
+
+namespace CelixSoftware.Logica
+{
+    internal class Autenticacion
+    {
+        private string usuario;
+        private string password;
+
+        public Autenticacion(string usuario, string passwrord)
+        {
+            this.usuario = usuario;
+            this.password = password;
+            Comparacion();
+        } 
+    }
+
+    private Comparacion()
+        {
+            
+        }
+}
