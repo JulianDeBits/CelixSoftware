3d999f9dcceb964f584b3cb4bea10128b801b705
JulianDeBits <julianquinterortiz@gmail.com>
2025-01-25 19:30:25 -0500

    Maquetacion V3.0

---
+++ App/RegistroVentaDB.cs
@@ -0,0 +1,26 @@
+public class Ventas
+{
+    private int idventa, idproducto, idvendedor, totalventa, cantidadarticulos;
+
+    private date fecha;
+
+    public Ventas(int idventa, int idproducto, int idvendedor, date fecha, int totalventa, int cantidadarticulos)
+    {
+        this.idventa = idventa;
+        this.idproducto = idproducto;
+        this.idvendedor = idvendedor;
+        this.totalventa = totalventa;
+        this.cantidadarticulos = cantidadarticulos;
+        this.fecha = fecha;
+    }
+
+    public void RegistrarVenta()
+    {
+
+    }
+
+    public void logicainventarios()
+    {
+
+    }
+}
