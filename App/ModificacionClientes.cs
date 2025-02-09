c2c5bbb4447f506b9ed2861e86b6bcbfe9899e84
JulianDeBits <julianquinterortiz@gmail.com>
2025-02-08 23:28:06 -0500

    RegistroClientes

---
+++ App/ModificacionClientes.cs
@@ -0,0 +1,26 @@
+public class ModificacionClientes
+{
+    private string Nombre, Nacionalidad, Correo, Direccion;
+    private int Documento;
+    public ModificacionClientes(string Nombre, string Nacionalidad, string Correo, string Direccion, int Documento)
+    {
+        this.Nombre = Nombre;
+        this.Nacionalidad = Nacionalidad;
+        this.Correo = Correo;
+        this.Direccion = Direccion;
+        this.Documento = Documento;
+    }
+
+    private void ObtenerDatos()
+    {
+
+    }
+    private void ModificarDatos()
+    {
+
+    }
+
+    private void EnviarDatos()
+    {
+
+    }
